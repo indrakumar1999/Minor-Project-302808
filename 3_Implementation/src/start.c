a492dca58dc6629d6e198a5b66fe13fb670cca63
DEEPAK KUMAR CHOUDHARY <86240095+deepakdanichy@users.noreply.github.com>
2021-07-08 01:26:53 +0530

    Add files via upload

---
+++ 3_Implementation/src/start.c
@@ -0,0 +1,32 @@
+/**
+ * @file start.c
+ * @author Deepak
+ * @brief To start the process and take input from user.
+ * @version 0.1
+ * @date 2021-07-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include<stdio.h>
+#include"hmgbpvs.h"
+
+/**
+ * @brief To start with Welcome meassage. 
+ * 
+ */
+
+void start() {
+ printf("\n**************************************************************");
+ printf("\n                  * ((Welcome Dear User)) *                   ");
+ printf("\n**************************************************************");
+ printf("\n \n");
+ printf("\n      HYBRID MICROGRID WITH BATTERY SUPPORTED PV SYSTEM       ");
+ printf("\n \n");
+ system("cls");
+ sleep(2);
+
+ //To take PV solar input.
+ 
+ prinf("Please provide PC Solar input");
